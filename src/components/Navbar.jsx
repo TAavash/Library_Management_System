ab863f57de8acba74e0831ef8d4a09360d877da9
sudarshan099 <sudarshanbam2021@gmail.com>
2024-06-16 12:27:31 +0545

    update3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-[10%] mx-[154px] mt-[10px] items-center justify-between">
+    <div className="fixed top-[10%] mx-[170px] mt-[10px] items-center justify-between">
       <div className="flex lg:w-[1132px] md:w-[800px] sm:w-[500px] h-[76px] rounded-[30px] bg-red-500 shadow-slate-500 shadow-md items-center justify-evenly">
         <button className="w-91 ">Home</button>
         <button className="w-91 ">Books</button>
