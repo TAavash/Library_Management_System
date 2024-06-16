0488597f216b2ffc7fd9ac05e8bfdac6e9a34f07
sudarshan099 <sudarshanbam2021@gmail.com>
2024-06-16 10:42:55 +0545

    update2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="fixed top-[15%] mx-[154px] mt-[10px] items-center justify-between">
+    <div className="fixed top-[10%] mx-[154px] mt-[10px] items-center justify-between">
       <div className="flex lg:w-[1132px] md:w-[800px] sm:w-[500px] h-[76px] rounded-[30px] bg-red-500 shadow-slate-500 shadow-md items-center justify-evenly">
-        <button className="w-91 h-44">Home</button>
-        <button className="w-91 h-44">Books</button>
-        <button className="w-91 h-44">Patrons</button>
-        <button className="w-91 h-44">Message</button>
-        <button className="w-91 h-44">Fines</button>
-        <button className="w-91 h-44">Others</button>
+        <button className="w-91 ">Home</button>
+        <button className="w-91 ">Books</button>
+        <button className="w-91 ">Patrons</button>
+        <button className="w-91 ">Message</button>
+        <button className="w-91 ">Fines</button>
+        <button className="w-91 ">Others</button>
       </div>
     </div>
   );
--- src/pages/Librarian/LibrarianDashboard.jsx
+++ src/pages/Librarian/LibrarianDashboard.jsx
@@ -10,6 +10,8 @@ export const LibrarianDashboard = () => {
       
       <Navbar />
 
+      
+
     </div>
 
 
