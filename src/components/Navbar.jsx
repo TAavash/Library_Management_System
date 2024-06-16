97d875f5ec24dacadcb13d75453495b8b382e205
Aavash Tamang <aavash.tamang@patancollege.edu.np>
2024-06-16 10:25:15 +0545

    updated more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-[65%] z-20 p-[5px] rounded-[30px] bg-red-500 shadow-slate-500 shadow-md fixed top-[15%] mx-[15%] mt-[10px]">
-      <div className="mx-auto justify-between content-stretch">
+      <div className="flex mx-auto justify-evenly content-stretch">
         <button className="w-[70px] h-[50px] rounded-r-full rounded-l-full hover:bg-blue-700 hover:underline text-white">Home</button>
         <button className="w-[70px] h-[50px] rounded-r-full rounded-l-full hover:bg-blue-700 hover:underline text-white">Books</button>
         <button className="w-[70px] h-[50px] rounded-r-full rounded-l-full hover:bg-blue-700 hover:underline text-white">Patrons</button>
