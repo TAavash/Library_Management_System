9ba57d53f5544053098a7460c3af67b8a1686ab2
sudarshan099 <sudarshanbam2021@gmail.com>
2024-07-03 11:03:37 +0545

    update

--- src/pages/Librarian/LibrarianDashboard.jsx
+++ src/pages/Librarian/LibrarianDashboard.jsx
@@ -67,60 +67,70 @@ export const LibrarianDashboard = () => {
 
   return (
 
-    <div >
+    <div>
       <NavNew />
+      <div className="min-h-screen h-auto bg-gray-200 py-[10px]">
+        <div className="flex justify-evenly gap-[20px] mx-[3%] my-16">
+          <button
 
-      <div className="flex justify-evenly ">
-        <button className="w-[150px] h-[120px] rounded-2xl bg-white hover:bg-red-600 active:bg-black shadow-slate-500 shadow-md mt-[100px] text-center"
-          onClick={handleAddBookClick}>
-          STUDENTS
-        </button>
-        <button className="w-[150px] h-[120px] rounded-2xl bg-white hover:bg-red-600 shadow-slate-500 shadow-md mt-[100px] text-center">
-          FACULTY MEMBERS
-        </button>
-        <button className="w-[150px] h-[120px] rounded-2xl bg-white hover:bg-red-600 shadow-slate-500 shadow-md mt-[100px] text-center">
-          LIBRARY ASSISTANT
-        </button>
-        <button className="w-[150px] h-[120px] rounded-2xl bg-white hover:bg-red-600 shadow-slate-500 shadow-md mt-[100px] text-center">
-          PROGRAM COORDINATOR
-        </button>
-      </div>
+            className="w-[150px] h-[120px] rounded-2xl bg-[#A3A3A3] hover:bg-red-600 shadow-slate-500 shadow-md mt-[100px]"
+            onClick={handleAddBookClick}>
+            STUDENTS
+          </button>
+          <button
 
-      <div className='mx-[170px] flex flex-col bg-white relative'>
+            className="w-[150px] h-[120px] rounded-2xl bg-white hover:bg-red-600 shadow-slate-500 shadow-md mt-[100px]"
+          >
+            FACULTY MEMBERS
+          </button>
+          <button
 
-        <div className='flex justify-center m-2'>
-          <div className="flex w-[500px] items-center gap-1 border rounded ">
-            <FiSearch className="h-5 w-5 ml-1" />
-            <input type="text" placeholder='Search' className='p-1 w-[500px]  border-hidden rounded-r-xl focus:outline-none' />
+            className="w-[150px] h-[120px] rounded-2xl bg-white hover:bg-red-600 shadow-slate-500 shadow-md mt-[100px]"
+          >
+            LIBRARY ASSISTANT
+          </button>
+          <button
+
+            className="w-[150px] h-[120px] rounded-2xl bg-white hover:bg-red-600 shadow-slate-500 shadow-md mt-[100px]"
+          >
+            PROGRAM COORDINATOR
+          </button>
+        </div>
+        <div className='mx-[170px] flex flex-col bg-white relative'>
+
+          <div className='flex justify-center m-2'>
+            <div className="flex w-[500px] items-center gap-1 border rounded ">
+              <FiSearch className="h-5 w-5 ml-1" />
+              <input type="text" placeholder='Search' className='p-1 w-[500px]  border-hidden rounded-r-xl focus:outline-none' />
+            </div>
+          </div>
+          <div className="h-[370px] overflow-scroll ">
+
+            <div className="absolute left-[85%] bottom-[15%] ">
+              <IoIosAddCircle className="h-16 w-16 cursor-pointer text-blue-500 hover:text-black "
+                onClick={handleAddIconClick} />
+            </div>
+            <table className='w-full border border-collapse bg-white'>
+              <tr>
+                <th className=' p-3 '>ID</th>
+                <th className=' p-3'>Name</th>
+                <th className=' p-3'>Status</th>
+                <th className=' p-3'>Joined Date</th>
+              </tr>
+              {tableData.map((person) => {
+                return (
+                  <tr key={person.id} onClick={handleRowClick} className="cursor-pointer hover:bg-gray-100">
+                    <td className='p-3'>{person.id}</td>
+                    <td className='p-3'>{person.fullname}</td>
+                    <td className='p-3'>{person.status}</td>
+                    <td className='p-3'>{person.joinDate}</td>
+                  </tr>)
+              })}
+
+            </table>
           </div>
         </div>
-        <div className="h-[360px] overflow-scroll ">
-
-          <div className="absolute left-[85%] bottom-[15%] ">
-            <IoIosAddCircle className="h-16 w-16 cursor-pointer text-blue-500 hover:text-black "
-              onClick={handleAddIconClick} />
-          </div>
-          <table className='w-full border border-collapse bg-white'>
-            <tr>
-              <th className=' p-3 '>ID</th>
-              <th className=' p-3'>Name</th>
-              <th className=' p-3'>Status</th>
-              <th className=' p-3'>Joined Date</th>
-            </tr>
-            {tableData.map((person) => {
-              return (
-                <tr key={person.id} onClick={handleRowClick} className="cursor-pointer hover:bg-gray-100">
-                  <td className='p-3'>{person.id}</td>
-                  <td className='p-3'>{person.fullname}</td>
-                  <td className='p-3'>{person.status}</td>
-                  <td className='p-3'>{person.joinDate}</td>
-                </tr>)
-            })}
-
-          </table>
-        </div>
       </div>
-
     </div>
 
 
