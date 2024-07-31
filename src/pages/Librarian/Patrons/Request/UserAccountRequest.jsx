2bcfb198a26dabe0357101193c3561e2899311a6
charitraa <stharabi9862187405@gmail.com>
2024-07-31 10:29:26 +0545

    fetching user list

--- src/pages/Librarian/Patrons/Request/UserAccountRequest.jsx
+++ src/pages/Librarian/Patrons/Request/UserAccountRequest.jsx
@@ -1,53 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { FaBell } from "react-icons/fa";
 import { IoSettingsSharp } from "react-icons/io5";
 import { useNavigate } from 'react-router-dom';
+import { userList } from '../../../../utils/Api';
 
 export default function UserAccountRequest() {
-
     const navigate = useNavigate();
+    const [AccountRequests, setAccountRequests] = useState([]);
 
     const handleAcceptClick = () => {
         navigate('/MemberRegistration');
     };
 
-    const AccountRequests = [
-        {
-            Name: "Sudarshan Bam",
-            email: "sudarshanbam2021@gmail.com",
-            status: "Account Request",
-        },
-        {
-            Name: "Sudarshan Bam",
-            email: "sudarshanbam2021@gmail.com",
-            status: "Account Request",
-        },
-        {
-            Name: "Sudarshan Bam",
-            email: "sudarshanbam2021@gmail.com",
-            status: "Account Request",
-        },
-        {
-            Name: "Sudarshan Bam",
-            email: "sudarshanbam2021@gmail.com",
-            status: "Account Request",
-        },
-        {
-            Name: "Sudarshan Bam",
-            email: "sudarshanbam2021@gmail.com",
-            status: "Account Request",
-        },
-        {
-            Name: "Sudarshan Bam",
-            email: "sudarshanbam2021@gmail.com",
-            status: "Account Request",
-        },
-        {
-            Name: "Sudarshan Bam",
-            email: "sudarshanbam2021@gmail.com",
-            status: "Account Request",
-        },
-    ];
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const list = await userList();
+                if (Array.isArray(list)) {
+                    setAccountRequests(list);
+                } else {
+                    console.error('Expected an array but got:', list);
+                }
+            } catch (error) {
+                console.error('Error fetching account requests:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <>
@@ -81,37 +61,41 @@ export default function UserAccountRequest() {
                         </div>
                     </div>
                 </div>
-                <div className='h-[80%] overflow-scroll'>
-
-                    {AccountRequests.map((item, index) => (
-                        <div className="flex border-2 mx-12 my-9 justify-evenly items-center">
-                            <div className="flex items-center w-2/4 p-4">
-                                <div>
-                                    <div className="text-lg font-medium text-gray-900">
-                                        {item.Name}
+                <div className="h-[80%] overflow-scroll">
+                    {AccountRequests.length > 0 ? (
+                        AccountRequests.map((item, index) => (
+                            <div key={index} className="flex border-2 mx-12 my-9 justify-evenly items-center">
+                                <div className="flex items-center w-2/4 p-4">
+                                    <div>
+                                        <div className="text-lg font-medium text-gray-900">
+                                            {item.username}
+                                        </div>
+                                        <div className="text-sm text-gray-500">{item.email}</div>
+                                        <div className="text-sm text-gray-500">{item.status}</div>
                                     </div>
-                                    <div className="text-sm text-gray-500">{item.email}</div>
-                                    <div className="text-sm text-gray-500">{item.status}</div>
+                                </div>
+                                <div className="flex">
+                                    <div className="mx-7 my-9">
+                                        <button className="my-1 border rounded-lg py-2 px-8 md:px-12 bg-black text-white"
+                                            onClick={handleAcceptClick}>
+                                            Accept
+                                        </button>
+                                    </div>
+                                    <div className="mx-7 my-9">
+                                        <button className="my-1 border rounded-lg py-2 px-8 md:px-12 bg-black text-white">
+                                            Reject
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="flex">
-                                <div className="mx-7 my-9">
-                                    <button className="my-1 border rounded-lg py-2 px-8 md:px-12 bg-black text-white"
-                                    onClick={handleAcceptClick}>
-                                        Accept
-                                    </button>
-                                </div>
-                                <div className="mx-7 my-9">
-                                    <button className="my-1 border rounded-lg py-2 px-8 md:px-12 bg-black text-white">
-                                        Reject
-                                    </button>
-                                </div>
-                            </div>
+                        ))
+                    ) : (
+                        <div className="flex justify-center items-center h-full">
+                            No account requests found.
                         </div>
-                    ))}
+                    )}
                 </div>
             </div>
-
         </>
     );
 }
