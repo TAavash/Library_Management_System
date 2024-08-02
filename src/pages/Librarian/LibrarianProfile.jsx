3279cf615ea5af1cd9a138613fb5724c99e4d7dc
Aavash Tamang <aavashlopchan@gmail.com>
2024-08-02 15:11:47 +0545

    profile data fetched based on id

--- src/pages/Librarian/LibrarianProfile.jsx
+++ src/pages/Librarian/LibrarianProfile.jsx
@@ -3,6 +3,7 @@ import { MdModeEditOutline } from "react-icons/md";
 import { AiOutlineMail, AiOutlinePhone, AiOutlineLock } from "react-icons/ai";
 import abhinab from "../../assets/abhinab.jpg";
 import NavNew from "../../components/NavNew";
+import { ToastContainer, toast } from "react-toastify";
 import { getMemberById } from "../../utils/Api";
 
 const LibrarianProfile = () => {
@@ -17,29 +18,47 @@ const LibrarianProfile = () => {
     address: "",
     email: "",
     mobile: "",
-    role_id: "",
+    // role_id: "",
+    role_name: "",
   });
-  
+
   const user_id = localStorage.getItem("user_id");
 
-  getMemberById(user_id);
-
   useEffect(() => {
-    const fetchData = async () => {
-      const data = {
-        firstName: "Ram",
-        lastName: "Yadav",
-        dob: "2004-02-10",
-        gender: "Male",
-        address: "123 Library St, City, Country",
-        email: "admin@example.com",
-        phoneNumber: "(123) 456-7890",
-      };
-      setProfileInfo(data);
+    const fetchMemberData = async () => {
+      try {
+        const memberData = await getMemberById(user_id);
+        let data = memberData.data;
+        if (memberData) {
+          setProfileInfo({
+            username: data.username || "",
+            password: data.password || "",
+            first_name: data.first_name || "",
+            last_name: data.last_name || "",
+            dob: data.dob || "",
+            gender: data.gender || "",
+            address: data.address || "",
+            email: data.email || "",
+            mobile: data.mobile || "",
+            // role_id: data.role_id || "",
+            role_name: data.role_name || "",
+          });
+        } else {
+          console.error("No user data found");
+          toast.error("No user data found. Please try again later.");
+        }
+      } catch (error) {
+        console.error("An error occurred while fetching user data:", error);
+        toast.error(
+          "An error occurred while fetching user data. Please try again later."
+        );
+      }
     };
 
-    fetchData();
-  }, []);
+    if (user_id) {
+      fetchMemberData();
+    }
+  }, [user_id]);
 
   const handleEditClick = () => {
     setIsEditing(!isEditing);
@@ -76,8 +95,8 @@ const LibrarianProfile = () => {
             />
           </div>
           <div className="ml-6 flex flex-col justify-center">
-            <h3 className="text-xl font-semibold text-gray-800">{`${profileInfo.firstName} ${profileInfo.lastName}`}</h3>
-            <p className="text-gray-600">Librarian</p>
+            <h3 className="text-xl font-semibold text-gray-800">{`${profileInfo.first_name} ${profileInfo.last_name}`}</h3>
+            <p className="text-gray-600">{`${profileInfo.role_name}`}</p>
           </div>
           <div className="ml-auto flex flex-col items-center space-y-2">
             <button className="w-40 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:outline-none text-sm font-semibold">
@@ -94,29 +113,32 @@ const LibrarianProfile = () => {
             <div className="w-1/2">
               <label
                 className="text-gray-600 font-semibold"
-                htmlFor="firstName"
+                htmlFor="first_name"
               >
                 First Name
               </label>
               <input
-                id="firstName"
+                id="first_name"
                 type="text"
-                name="firstName"
-                value={profileInfo.firstName}
+                name="first_name"
+                value={profileInfo.first_name}
                 onChange={handleInputChange}
                 className="w-full p-2 border rounded"
                 disabled={!isEditing}
               />
             </div>
             <div className="w-1/2">
-              <label className="text-gray-600 font-semibold" htmlFor="lastName">
+              <label
+                className="text-gray-600 font-semibold"
+                htmlFor="last_name"
+              >
                 Last Name
               </label>
               <input
-                id="lastName"
+                id="last_name"
                 type="text"
-                name="lastName"
-                value={profileInfo.lastName}
+                name="last_name"
+                value={profileInfo.last_name}
                 onChange={handleInputChange}
                 className="w-full p-2 border rounded"
                 disabled={!isEditing}
@@ -174,19 +196,16 @@ const LibrarianProfile = () => {
               </div>
             </div>
             <div className="w-1/2">
-              <label
-                className="text-gray-600 font-semibold"
-                htmlFor="phoneNumber"
-              >
+              <label className="text-gray-600 font-semibold" htmlFor="mobile">
                 Phone Number
               </label>
               <div className="flex items-center border rounded p-2">
                 <AiOutlinePhone className="text-gray-500 mr-2" />
                 <input
-                  id="phoneNumber"
+                  id="mobile"
                   type="text"
-                  name="phoneNumber"
-                  value={profileInfo.phoneNumber}
+                  name="mobile"
+                  value={profileInfo.mobile}
                   onChange={handleInputChange}
                   className="w-full focus:outline-none"
                   disabled={!isEditing}
@@ -212,61 +231,32 @@ const LibrarianProfile = () => {
           </div>
           <div className="flex space-x-4">
             <div className="w-1/2">
-              <label
-                className="text-gray-600 font-semibold"
-                htmlFor="currentPassword"
-              >
+              <label className="text-gray-600 font-semibold" htmlFor="gender">
+                Username
+              </label>
+              <input
+                id="username"
+                type="text"
+                name="username"
+                value={profileInfo.username}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded"
+                disabled={!isEditing}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-4">
+            <div className="w-1/2">
+              <label className="text-gray-600 font-semibold" htmlFor="password">
                 Current Password
               </label>
               <div className="flex items-center border rounded p-2">
                 <AiOutlineLock className="text-gray-500 mr-2" />
                 <input
-                  id="currentPassword"
+                  id="password"
                   type="password"
-                  name="currentPassword"
-                  value={profileInfo.currentPassword}
-                  onChange={handleInputChange}
-                  className="w-full focus:outline-none"
-                  disabled={!isEditing}
-                />
-              </div>
-            </div>
-            <div className="w-1/2">
-              <label
-                className="text-gray-600 font-semibold"
-                htmlFor="newPassword"
-              >
-                New Password
-              </label>
-              <div className="flex items-center border rounded p-2">
-                <AiOutlineLock className="text-gray-500 mr-2" />
-                <input
-                  id="newPassword"
-                  type="password"
-                  name="newPassword"
-                  value={profileInfo.newPassword}
-                  onChange={handleInputChange}
-                  className="w-full focus:outline-none"
-                  disabled={!isEditing}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex space-x-4">
-            <div className="w-full">
-              <label
-                className="text-gray-600 font-semibold"
-                htmlFor="confirmPassword"
-              >
-                Confirm New Password
-              </label>
-              <div className="flex items-center border rounded p-2">
-                <AiOutlineLock className="text-gray-500 mr-2" />
-                <input
-                  id="confirmPassword"
-                  type="password"
-                  name="confirmPassword"
-                  value={profileInfo.confirmPassword}
+                  name="password"
+                  value={profileInfo.password}
                   onChange={handleInputChange}
                   className="w-full focus:outline-none"
                   disabled={!isEditing}
@@ -300,6 +290,7 @@ const LibrarianProfile = () => {
           )}
         </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
--- src/pages/User/UserProfile.jsx
+++ src/pages/User/UserProfile.jsx
@@ -1,105 +1,294 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import Profile from "../../assets/profilepicture.jpeg";
-import Background from "../../assets/profilebg.png";
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { MdModeEditOutline } from "react-icons/md";
+import { AiOutlineMail, AiOutlinePhone, AiOutlineLock } from "react-icons/ai";
+import abhinab from "../../assets/abhinab.jpg";
+import { ToastContainer, toast } from "react-toastify";
+import { getMemberById } from "../../utils/Api";
 
 const UserProfile = () => {
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    localStorage.removeItem("access_token");
-    navigate(`/`);
+  const [isEditing, setIsEditing] = useState(false);
+  const [profileInfo, setProfileInfo] = useState({
+    username: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+    dob: "",
+    gender: "",
+    address: "",
+    email: "",
+    mobile: "",
+    // role_id: "",
+    role_name: "",
+  });
+
+  const user_id = localStorage.getItem("user_id");
+
+  useEffect(() => {
+    const fetchMemberData = async () => {
+      try {
+        const memberData = await getMemberById(user_id);
+        let data = memberData.data;
+        if (memberData) {
+          setProfileInfo({
+            username: data.username || "",
+            password: data.password || "",
+            first_name: data.first_name || "",
+            last_name: data.last_name || "",
+            dob: data.dob || "",
+            gender: data.gender || "",
+            address: data.address || "",
+            email: data.email || "",
+            mobile: data.mobile || "",
+            // role_id: data.role_id || "",
+            role_name: data.role_name || "",
+          });
+        } else {
+          console.error("No user data found");
+          toast.error("No user data found. Please try again later.");
+        }
+      } catch (error) {
+        console.error("An error occurred while fetching user data:", error);
+        toast.error(
+          "An error occurred while fetching user data. Please try again later."
+        );
+      }
+    };
+
+    if (user_id) {
+      fetchMemberData();
+    }
+  }, [user_id]);
+
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
   };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProfileInfo({
+      ...profileInfo,
+      [name]: value,
+    });
+  };
+
+  const handleSaveClick = async () => {
+    const saveData = async () => {
+      console.log("Saving data...", profileInfo);
+    };
+
+    await saveData();
+    setIsEditing(false);
+  };
+
   return (
-    <div className="relative">
-      {/* Background Image */}
-      <div
-        className="h-[300px] bg-cover bg-center"
-        style={{ backgroundImage: `url(${Background})` }}
-      ></div>
-
-      {/* Container with Profile and Info */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 bg-white shadow-gray-600 shadow-xl rounded-lg p-6 pb-3 w-full max-w-4xl">
-        {/* Profile Section */}
-        <div className="bg-gradient-to-r from-black via-gray-500 to-white p-4 flex items-center rounded-lg mb-6">
-          <img
-            src={Profile}
-            className="w-24 h-24 object-cover rounded-full border-4 border-white mr-4"
-            alt="profile"
-          />
-          <div className="text-white">
-            <h2 className="text-2xl font-semibold">Abhinab Prajapati</h2>
-            <p className="text-lg">Student</p>
+    <div className="min-h-screen flex flex-col items-center bg-gray-100 w-screen">
+      <div className="flex-grow w-full bg-white shadow-lg rounded-lg mt-10 overflow-hidden flex flex-col p-8">
+        <h2 className="text-3xl font-bold text-gray-800 mb-6">User Profile</h2>
+        <div className="flex items-center mb-6">
+          <div className="w-32 h-32 rounded-full overflow-hidden border-4 border-white relative">
+            <img
+              className="w-full h-full object-cover"
+              src={abhinab}
+              alt="User Profile"
+            />
           </div>
-        </div>
-
-        {/* Profile Information */}
-        <div className="space-y-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="bg-gray-100 p-3 rounded-lg shadow-md">
-              <h3 className="text-base font-semibold text-gray-800 mb-1">
-                First Name
-              </h3>
-              <p className="text-gray-700 text-sm">Abhinab</p>
-            </div>
-            <div className="bg-gray-100 p-3 rounded-lg shadow-md">
-              <h3 className="text-base font-semibold text-gray-800 mb-1">
-                Last Name
-              </h3>
-              <p className="text-gray-700 text-sm">Prajapati</p>
-            </div>
-            <div className="bg-gray-100 p-3 rounded-lg shadow-md">
-              <h3 className="text-base font-semibold text-gray-800 mb-1">
-                Gender
-              </h3>
-              <p className="text-gray-700 text-sm">Male</p>
-            </div>
-            <div className="bg-gray-100 p-3 rounded-lg shadow-md">
-              <h3 className="text-base font-semibold text-gray-800 mb-1">
-                Address
-              </h3>
-              <p className="text-gray-700 text-sm">Patan</p>
-            </div>
-            <div className="bg-gray-100 p-3 rounded-lg shadow-md">
-              <h3 className="text-base font-semibold text-gray-800 mb-1">
-                Email
-              </h3>
-              <p className="text-gray-700 text-sm">abhinabsuii@mail.com</p>
-            </div>
-            <div className="bg-gray-100 p-3 rounded-lg shadow-md">
-              <h3 className="text-base font-semibold text-gray-800 mb-1">
-                Mobile
-              </h3>
-              <p className="text-gray-700 text-sm">977-9812121212</p>
-            </div>
-            <div className="bg-gray-100 p-3 rounded-lg shadow-md">
-              <h3 className="text-base font-semibold text-gray-800 mb-1">
-                Library Card Number
-              </h3>
-              <p className="text-gray-700 text-sm">2214618</p>
-            </div>
+          <div className="ml-6 flex flex-col justify-center">
+            <h3 className="text-xl font-semibold text-gray-800">{`${profileInfo.first_name} ${profileInfo.last_name}`}</h3>
+            <p className="text-gray-600">{`${profileInfo.role_name}`}</p>
           </div>
-          <div className="flex gap-3 justify-center">
-            <a href="#" className="text-blue-600 hover:text-blue-800 text-xl">
-              <FaFacebook />
-            </a>
-            <a href="#" className="text-blue-400 hover:text-blue-600 text-xl">
-              <FaTwitter />
-            </a>
-            <a href="#" className="text-pink-600 hover:text-pink-800 text-xl">
-              <FaInstagram />
-            </a>
-          </div>
-          <div className="flex justify-center items-center">
-            <button
-              onClick={handleLogout}
-              className="h-fit w-[100px] rounded-md text-red-600 border-2 border-red-600 hover:bg-slate-300 active:bg-black text-2xl p-[2px] px-[3px]"
-            >
-              Logout
+          <div className="ml-auto flex flex-col items-center space-y-2">
+            <button className="w-40 px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 focus:outline-none text-sm font-semibold">
+              Upload New Photo
+            </button>
+            <button className="w-40 px-4 py-2 border border-gray-500 text-gray-500 rounded-lg hover:bg-gray-200 focus:outline-none text-sm font-semibold">
+              Delete
             </button>
           </div>
         </div>
+
+        <div className="flex flex-col space-y-4">
+          <div className="flex space-x-4">
+            <div className="w-1/2">
+              <label
+                className="text-gray-600 font-semibold"
+                htmlFor="first_name"
+              >
+                First Name
+              </label>
+              <input
+                id="first_name"
+                type="text"
+                name="first_name"
+                value={profileInfo.first_name}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded"
+                disabled={!isEditing}
+              />
+            </div>
+            <div className="w-1/2">
+              <label
+                className="text-gray-600 font-semibold"
+                htmlFor="last_name"
+              >
+                Last Name
+              </label>
+              <input
+                id="last_name"
+                type="text"
+                name="last_name"
+                value={profileInfo.last_name}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded"
+                disabled={!isEditing}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-4">
+            <div className="w-full">
+              <label className="text-gray-600 font-semibold" htmlFor="dob">
+                Date of Birth
+              </label>
+              <input
+                id="dob"
+                type="text"
+                name="dob"
+                value={profileInfo.dob}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded"
+                disabled={!isEditing}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-4">
+            <div className="w-full">
+              <label className="text-gray-600 font-semibold" htmlFor="address">
+                Address
+              </label>
+              <input
+                id="address"
+                type="text"
+                name="address"
+                value={profileInfo.address}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded"
+                disabled={!isEditing}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-4">
+            <div className="w-1/2">
+              <label className="text-gray-600 font-semibold" htmlFor="email">
+                Email Address
+              </label>
+              <div className="flex items-center border rounded p-2">
+                <AiOutlineMail className="text-gray-500 mr-2" />
+                <input
+                  id="email"
+                  type="email"
+                  name="email"
+                  value={profileInfo.email}
+                  onChange={handleInputChange}
+                  className="w-full focus:outline-none"
+                  disabled={!isEditing}
+                />
+              </div>
+            </div>
+            <div className="w-1/2">
+              <label className="text-gray-600 font-semibold" htmlFor="mobile">
+                Phone Number
+              </label>
+              <div className="flex items-center border rounded p-2">
+                <AiOutlinePhone className="text-gray-500 mr-2" />
+                <input
+                  id="mobile"
+                  type="text"
+                  name="mobile"
+                  value={profileInfo.mobile}
+                  onChange={handleInputChange}
+                  className="w-full focus:outline-none"
+                  disabled={!isEditing}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex space-x-4">
+            <div className="w-1/2">
+              <label className="text-gray-600 font-semibold" htmlFor="gender">
+                Gender
+              </label>
+              <input
+                id="gender"
+                type="text"
+                name="gender"
+                value={profileInfo.gender}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded"
+                disabled={!isEditing}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-4">
+            <div className="w-1/2">
+              <label className="text-gray-600 font-semibold" htmlFor="gender">
+                Username
+              </label>
+              <input
+                id="username"
+                type="text"
+                name="username"
+                value={profileInfo.username}
+                onChange={handleInputChange}
+                className="w-full p-2 border rounded"
+                disabled={!isEditing}
+              />
+            </div>
+          </div>
+          <div className="flex space-x-4">
+            <div className="w-1/2">
+              <label className="text-gray-600 font-semibold" htmlFor="password">
+                Current Password
+              </label>
+              <div className="flex items-center border rounded p-2">
+                <AiOutlineLock className="text-gray-500 mr-2" />
+                <input
+                  id="password"
+                  type="password"
+                  name="password"
+                  value={profileInfo.password}
+                  onChange={handleInputChange}
+                  className="w-full focus:outline-none"
+                  disabled={!isEditing}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="mt-6 flex justify-end space-x-4">
+          {isEditing && (
+            <>
+              <button
+                className="px-4 py-2 border border-gray-500 text-gray-500 rounded-lg hover:bg-gray-200 font-semibold"
+                onClick={handleEditClick}
+              >
+                Cancel
+              </button>
+              <button
+                className="px-4 py-2 bg-gray-500 text-white rounded-lg hover:bg-gray-600 font-semibold"
+                onClick={handleSaveClick}
+              >
+                Save Changes
+              </button>
+            </>
+          )}
+          {!isEditing && (
+            <MdModeEditOutline
+              className="text-2xl cursor-pointer rounded-lg hover:bg-gray-200 "
+              onClick={handleEditClick}
+            />
+          )}
+        </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
